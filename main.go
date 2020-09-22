6e7b0241bd85702dfd8aedf473445a4827ab3ec1
pixel-big <2282725505@qq.com>
2020-09-22 18:57:47 +0800

    BeeGoProject_v1.0.0

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	_ "BeeGoProjet_v1.0.0/routers"
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	beego.Run()
+}
+
---
+++ routers/router.go
@@ -0,0 +1,10 @@
+package routers
+
+import (
+	"BeeGoProjet_v1.0.0/controllers"
+	"github.com/astaxie/beego"
+)
+
+func init() {
+    beego.Router("/", &controllers.MainController{})
+}
